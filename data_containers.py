8ffc5a965fdfb6ff256bae606d5a8192f934b0d0
Oeyvind <obrandts@gmail.com>
2024-11-29 14:25:23 +0100

    automatic assignment of prob logic indices from parameter description in datacontainers.py

--- data_containers.py
+++ data_containers.py
@@ -9,6 +9,7 @@ import numpy as np
 np.set_printoptions(precision=2)
 
 max_events = 100 # 100 for test, 10.000 for small scale production
+
 # parameter names and their indices in the corpus
 pnum_corpus = {
   'index': 0, # register indices for data points currently in use
@@ -31,37 +32,39 @@ corpus = np.zeros((max_events,nparms_corpus), dtype=np.float32) # float32 faster
 # parameter names and max_order in the probabilistic logic module
 # zero order just means give us all indices where the value occurs
 # higher orders similar to markov order
-prob_parms = {
-  'ratio_best': [2, None, [0,1,2]],
-  'ratio_2nd_best': [2, None, [3,4,5]]}
+prob_parms_description = {
+  'ratio_best': 2,
+  'ratio_2nd_best': 2}
 
-'''# test set
-pnum_prob = {
-  'ratio_best': 0, # 'zeroeth order' just means give us all indices where the value occurs
-  'ratio_best_1order': 1, # first order markovian lookup
-  'ratio_best_2order': 2,
-  'ratio_2nd_best': 3,
-  'ratio_2nd_best_1order': 4,
-  'ratio_2nd_best_2order': 5,
-  'phrase_num': 6} # the rest is placeholders, to be implemented
+# set up prob_parms as dict with format: [order, prob_encoder instance, [list of prob logic indices]]
+# prob_encoder instance will be updated when the encoder is instantiated
+# prob_logic indices corresponds to weights for each dimension in probabilistic logic
+i = 0
+prob_parms = {}
+for key,value in prob_parms_description.items():
+  indices = []
+  for j in range(value+1):
+    indices.append(i)
+    i += 1
+  prob_parms[key] = [value, None, indices]
+
+
+'''
+# test set
+prob_parms_description = {
+  'ratio_best': 2, 
+  'ratio_2nd_best': 2,
+  'phrase_num': 1} 
 '''
 
-# full set
 '''
-pnum_prob = {
-  'ratio_best': 0, # 'zeroeth order' just means give us all indices where the value occurs
-  'ratio_best_1order': 1, # first order markovian lookup
-  'ratio_best_2order': 2,
-  'ratio_best_3order': 3,
-  'ratio_best_4order': 4,
-  'ratio_2nd_best': 5,
-  'ratio_2nd_best_1order': 6,
-  'ratio_2nd_best_2order': 7,
-  'ratio_2nd_best_3order': 8,
-  'ratio_2nd_best_4order': 9,
-  'phrase_num': 10, # the rest is placeholders, to be implemented
-  'downbeat': 11,
-  'amp_relative': 12,
-  'pitch_relative': 13,
+# full set example
+prob_parms_description = {
+  'ratio_best': 4, 
+  'ratio_2nd_best': 4,
+  'phrase_num': 1,
+  'downbeat': 0,
+  'amp_relative': 2,
+  'pitch_relative': 2,
 }
 '''
