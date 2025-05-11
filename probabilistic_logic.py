3bf0bf5cae75a27d4446c256d15d7bd753df7197
Oeyvind <obrandts@gmail.com>
2025-05-11 11:06:34 +0200

    prob logic rewritten and tested. Now using indexing of the current value instead of the previous value. Sequence transitions will be the same, but also allows backwards prediction (if that is a useable term)

--- probabilistic_logic.py
+++ probabilistic_logic.py
@@ -27,57 +27,50 @@ class Probabilistic_encoder:
         self.wraparound = 1 # wraparound or random choice on dead end
         self.wraparound_index_container = np.copy(self.empty_index_container)
         self.wraparound_index_container[max_order] = 1 # to use for avoiding dead ends
-        self.previous_item = None 
+        #self.previous_item = None 
         self.name = name # just for debugging printing
 
     def analyze(self, item, index):
-        print('analyze', index, item, self.previous_item)
-        if self.previous_item == None: # first item received is treated differently
-            self.previous_item = item
-            return
-        else: # all next items are analyzed, stored as possible successors to the previous note
-            if self.previous_item not in self.stm.keys():
-                index_container = np.copy(self.empty_index_container)
-                index_container[index+self.max_order] = 1
-                self.stm[self.previous_item] = index_container
-            else:
-                self.stm[self.previous_item][index+self.max_order] = 1
-            self.previous_item = item
+        # "analysis" is just storing the indices at which the item occurs
+        if item not in self.stm.keys():
+            index_container = np.copy(self.empty_index_container)
+            index_container[index+self.max_order] = 1
+            self.stm[item] = index_container
+        else:
+            self.stm[item][index+self.max_order] = 1
 
-    def next_items(self, previous=None):
+    def get_item_indices(self, item=None):
         # as we are live recording items for analysis, dead ends are likely, and needs to be dealt with
-        if previous and (previous not in self.stm.keys()):
+        if item and (item not in self.stm.keys()):
             if self.wraparound == 1:
-                #print(f'Prob_encoder: {self.name} dead end at key {previous}, wrap around ')
-                #print(f'key: {previous}, allkeys: {self.stm.keys()}')
+                #print(f'Prob_encoder: {self.name} dead end at key {item}, wrap around ')
+                #print(f'key: {item}, allkeys: {self.stm.keys()}')
                 return self.wraparound_index_container
             else:
-                #print(f'Prob_encoder: {self.name} dead end at key {previous}, choose randomly')
-                #print(f'key: {previous}, allkeys: {self.stm.keys()}')
+                #print(f'Prob_encoder: {self.name} dead end at key {item}, choose randomly')
+                #print(f'key: {item}, allkeys: {self.stm.keys()}')
                 return self.empty_index_container
         
         if len(self.stm.keys()) == 0:
             print('Empty Prob sequence')
             return [-1.0]
-        # for the very first item, if we do not have any previous note, so let's choose one randomly
+        # if we do not ask for a specific item, choose one randomly
         # same applies if we ask for a nonexisting key
-        if (previous == None) or (previous not in self.stm.keys()):
+        if (item == None) or (item not in self.stm.keys()):
             #print(f'Prob_encoder: {self.name} previous {previous} not in stm.keys')
-            alternatives = self.empty_index_container
+            indices = self.empty_index_container
         else:
-            alternatives = self.stm[previous] # get an index container of possible next items
-        return alternatives
+            indices = self.stm[item] # get an index container of possible next items
+        return indices
     
     def clear(self):
         self.stm = {}
-        self.previous_item = None 
     
     def clear_phrase(self, indices):
         # clear last recorded phrase
         for key in self.stm.keys():
             for i in indices:
                 self.stm[key][i+self.max_order] 
-        self.previous_item == None
 
         
 class Probabilistic_logic:
@@ -98,7 +91,7 @@ class Probabilistic_logic:
         print('number of parameters in probabilistic logic:', self.numparms)
         
         prob_history = []
-        for i in range(self.max_order): prob_history.append(None)
+        for i in range(self.max_order): prob_history.append(-1)
         self.no_prob_history = prob_history.copy()
         self.prob_history = []
         for i in range(max_voices):
@@ -119,10 +112,8 @@ class Probabilistic_logic:
         self.prob = np.zeros(self.maxsize)
         
     def analyze_single_event(self, i):
-        #print(f'analyze: {self.dc.corpus[i]}')
         for parm in self.dc.prob_parms.keys():
             pe = self.dc.prob_parms[parm][1]
-            #print(f'pe.analyze: {pe.name}, {self.dc.corpus[i, self.dc.pnum_corpus[parm]]}')
             pe.analyze(self.dc.corpus[i, self.dc.pnum_corpus[parm]], i)
         self.current_datasize += 1
         self.indices = self.dc.corpus[:self.current_datasize, self.dc.pnum_corpus['index']]
@@ -153,14 +144,15 @@ class Probabilistic_logic:
         history = history[1:]+history[:1] #rotate
         history[-1] = int(new_item) # add most recent INDEX last in list
         for i in range(len(history)-2): # not process the last (newest)
-            if not history[i]:
+            if history[i] < 0:
                 if history[i+1]:
                     history[i] = history[i+1]-1 # if there is a gap in the history, smooth out
         return history
 
     def generate(self, query, voice=1, temperature=0.2):
+        # get the next_item from the probabilistic sequence generation
+        # the request_next_item is used as a filter/bias
         next_item_index, request_next_item = query
-        #print('query', query)
         temperature_coef = self.set_temperature(temperature)
         
         # need to update and keep track of previous events for higher orders
@@ -172,12 +164,12 @@ class Probabilistic_logic:
             for ord in range(1,self.dc.prob_parms[parm][0]+1): # will skip for specific request (order 0)
                 w_index = self.dc.prob_parms[parm][2][ord] #prob weight index
                 if self.weights[w_index] != 0:
-                    offset = self.max_order+1-ord
-                    if not self.prob_history[voice-1][-ord]:
+                    offset = self.max_order-ord
+                    if self.prob_history[voice-1][-ord] < 0:
                         query_item = None
                     else:
                         query_item = self.dc.corpus[self.prob_history[voice-1][-ord], self.dc.pnum_corpus[parm]]
-                    self.indices_prob_temp = pe.next_items(query_item)[offset:self.current_datasize+offset]
+                    self.indices_prob_temp = pe.get_item_indices(query_item)[offset:self.current_datasize+offset]
                     self.indx_container[:self.current_datasize, w_index] = self.indices_prob_temp[:self.current_datasize]
 
         # Scale by weights for each prob dimension and sum: dot product indx_container and weight. Then adjust temperature
@@ -189,13 +181,10 @@ class Probabilistic_logic:
             print(f'Prob encoder zero probability from query {query}, choose one at random')
             self.prob = np.ones(self.current_datasize)
 
-        #print('prob and mask:')
-        #print(self.prob)
         # if we request a specific item, handle this here 
         if request_next_item[0] != None:
             request_parm, request_code, request_weight = request_next_item
             self.set_request_mask(request_parm, request_code)
-            #print(self.request_mask[:self.current_datasize])
             if request_weight == 1:
                 self.prob *= self.request_mask[:self.current_datasize]
             else:        
@@ -205,7 +194,6 @@ class Probabilistic_logic:
                 self.prob += self.request_mask[:self.current_datasize] # just use mask
         sumprob = np.sum(self.prob)
         self.prob = self.prob/sumprob #normalize sum to 1
-        #print('prob again\n', self.prob)
         next_item_index = int(np.random.choice(self.indices,p=self.prob))
         return next_item_index
 
@@ -230,8 +218,8 @@ class Probabilistic_logic:
                 # THIS SHOULD BE OPTIONAL, make argument selector to enable (if no exact values found, return flat probability)
                 request_next_item_closest = keys[np.abs(val-keys).argmin()]
                 print(f'closest val: {request_next_item_closest}')
-                offset = self.max_order+1
-                request = pe.next_items(request_next_item_closest)[offset:self.current_datasize+offset]
+                offset = self.max_order
+                request = pe.get_item_indices(request_next_item_closest)[offset:self.current_datasize+offset]
                 self.request_mask[:self.current_datasize] += request[:self.current_datasize]
         elif request_type == 'next':
             if request_parm == 'index':
@@ -245,17 +233,17 @@ class Probabilistic_logic:
             # For request code 'gradient', '>' or '<', we need to look at the values in the corpus rather than the index containers
             #   A gradient can be aligned to prefer low values or high values, with increasing probability along the gradient
             #   The request mask in this case is not a simple mask, but a floating point probability. Still using the same size and format as the request mask.
-            #   Special treatment of relative pitch, where we might want to request large (or small) intervals, regardless of sign (up or down intervals)
-            #   In that case, use absolute values of interval (request code = 'gr_abs').
+            #   We might want to request large (or small) values, regardless of sign, 
+            #   in that case use absolute values of interval (request code = 'gr_abs').
             # Request code < or > works similarly, but gives a binary mask (over/under threshold)
             pvalues = self.dc.corpus[:, self.dc.pnum_corpus[request_parm]][:self.current_datasize]
             if (request_type == '>') or (request_type == '<'):
                 if request_type == '>':
-                    pvalues = np.ma.masked_greater_equal(pvalues,val).mask
+                    pvalues = np.ma.masked_greater(pvalues,val).mask
                 else:
-                    pvalues = np.ma.masked_less_equal(pvalues,val).mask
+                    pvalues = np.ma.masked_less(pvalues,val).mask
             else: # if gradient
-                gradient_shape = request_code[1][0] # request value in gui
+                gradient_shape = request_code[1][0] # a bit ugly, but we re-use the request value as gradient shape here. It is not needed as request value in this specific case.
                 if request_type == 'gr_abs':
                     pvalues = np.abs(pvalues)
                 amin = np.amin(pvalues)
@@ -333,7 +321,7 @@ class Datacontainer_test():
         self.corpus = np.zeros((self.max_events, self.nparms_corpus), dtype=np.float32) # float32 faster than int or float64
         
         if random_population == 0:
-            self.list_val1 = [1,2,2,1,3,4,5, 3,4,5,-1,-2] 
+            self.list_val1 = [1,2,2,1,3,4,2, 3,4,5,-1,-2] 
             self.list_val2 = [1,1,1,1,1,1,1, 2,2,2,2,2] 
         else:
             self.list_val1 = np.random.randint(-10,10,random_population)
@@ -354,10 +342,12 @@ def encoder_test():
         pe.analyze(data[i],i)
         indices[i+max_order] = i
         current_datasize += 1
+        '''
     print('*stm*:')
     for k,v in pe.stm.items():
         print(k,v)
-    '''
+        '''
+    
     v = ('A')
     temperature_coef = 1
     for i in range(10):
@@ -371,25 +361,27 @@ def encoder_test():
         next_item_index = int(np.random.choice(indices,p=prob))
         v = data[next_item_index]
         print(v)
-    '''
+    
 def basic_test():
-    max_events = 1000
+    max_events = 20
     dc = Datacontainer_test(max_events)
     pl = Probabilistic_logic(dc, max_size=max_events, max_order=2, max_voices=2)
     for i in range(len(dc.list_val1)):
         dc.corpus[i, dc.pnum_corpus['index']] = i
         pl.analyze_single_event(i)
     print('done analyzing')
+    print('corpus')
+    print(dc.corpus)
     
     #generate
-    pl.set_weights_pname('val1', 1.5)
-    temperature = 0.2 # low (<1.0) is deterministic, high (>1.0) is more random
+    pl.set_weights_pname('val2', 0, printit=True)
+    pl.set_weights_pname('val1', 2, printit=True)
+    temperature = 1 # low (<1.0) is deterministic, high (>1.0) is more random
     start_index = 0#np.random.choice(indices)
     next_item = dc.corpus[start_index, dc.pnum_corpus['val1']]
     # for debug only
     for parm in dc.prob_parms.keys():
         pe = dc.prob_parms[parm][1]
-        # FIX HERE
         print(f'stm for {parm}')
         for key, value in pe.stm.items():
             print(key, value[2:pl.current_datasize+pl.max_order])
@@ -401,9 +393,10 @@ def basic_test():
     i = 0
     voice = 1
     while i < 10:
+        previous = query[0]
         next_item_index = pl.generate(query, voice, temperature) #query probabilistic encoders for next event and update query for next iteration
         query[0] = next_item_index
-        print(f"the next item is  {dc.corpus[next_item_index,dc.pnum_corpus['val1']]} at index {next_item_index}, prob {pl.prob}")
+        print(f"the next item is  {dc.corpus[next_item_index,dc.pnum_corpus['val1']]} at index {next_item_index}, prob from {dc.corpus[previous,dc.pnum_corpus['val1']]}: {pl.prob}")
         i += 1
     print(f'generated {i} items')
     
@@ -412,10 +405,10 @@ def basic_test():
     start_index = 1
     next_item = dc.corpus[start_index, dc.pnum_corpus['val1']]
     print(f'The first item is {next_item} at index {start_index}')
-    request_item = 'index'
-    request_type = 'values'
-    request_value = 1
-    request_weight = 0.5
+    request_item = 'val1'
+    request_type = '>'
+    request_value = 2.1
+    request_weight = 1
     if request_item: 
         query = [start_index, [request_item, [request_type, [request_value]], request_weight]]
     else: 
@@ -424,83 +417,86 @@ def basic_test():
     voice = 2
     while i < 10:
         next_item_index = pl.generate(query, voice) #query probabilistic encoders for next event and update query for next iteration
-        if request_item: 
+        if (request_item == 'index') and (request_type == '=='):
             request_value = next_item_index+1
             query = [next_item_index, [request_item, [request_type, [request_value]], request_weight]] #request index, and use next_item_index as the value to ask for
-        else: query = [next_item_index, [None, [0, [0]], 0]]
         print(f"the next item is  {dc.corpus[next_item_index, dc.pnum_corpus['val1']]} at index {next_item_index}, prob {pl.prob}")
         i += 1
     print(f'generated {i} items')
-
-        
-    print(pl.current_datasize)
-    #request_next_item = ['index', 1, 0.5]
-    #request_next_item = ['val1', ['values', [2.3]], 1] # list of one value
-    request_next_item = ['val1', ['values' ,[1,3]], 1] # list of values
-    #request_next_item = ['val1', ['>', [2]], 1] #mask high values, e.g. all x > zero
-    mask = pl.get_request_mask(request_next_item)
-    print('mask of values 1 and 3:\n', mask)
+    
 
 def request_dev():
-    max_events = 10
-    dc = Datacontainer_test(max_events, random_population=8)
+    #max_events = 10
+    #dc = Datacontainer_test(max_events, random_population=8)
+    max_events = 20
+    dc = Datacontainer_test(max_events)
     pl = Probabilistic_logic(dc, max_size=max_events, max_order=2, max_voices=2)
     for i in range(len(dc.list_val1)):
         dc.corpus[i,dc.pnum_corpus['index']] = i
         pl.analyze_single_event(i)
     print('done analyzing')
     print(f'corpus:\n{dc.corpus}')
-
+    
     request_next_item = ['index', ['==' ,[1]], 1]
     print(f'request mask for {request_next_item}:')
-    mask = pl.get_request_mask(request_next_item)
-    print(f'mask: {mask}')
+    request_parm, request_code, request_weight = request_next_item
+    pl.set_request_mask(request_parm, request_code)
+    print(f'mask: {pl.request_mask[:pl.current_datasize]}')
     
-    request_next_item = ['val1', ['==', [2.3]], 1] # list of one value
+    request_next_item = ['val1', ['==', [2.6]], 1] # list of one value
     print(f'request mask for {request_next_item}:')
-    mask = pl.get_request_mask(request_next_item)
-    print(f'mask: {mask}')
+    request_parm, request_code, request_weight = request_next_item
+    pl.set_request_mask(request_parm, request_code)
+    print(f'mask: {pl.request_mask[:pl.current_datasize]}')
 
     request_next_item = ['val1', ['==' ,[1]], 1] # list of values
     print(f'request mask for {request_next_item}:')
-    mask = pl.get_request_mask(request_next_item)
-    print(f'mask: {mask}')
+    request_parm, request_code, request_weight = request_next_item
+    pl.set_request_mask(request_parm, request_code)
+    print(f'mask: {pl.request_mask[:pl.current_datasize]}')
 
     request_next_item = ['val1', ['>', [2]], 1] #mask high values, e.g. all x > zero
     print(f'request mask for {request_next_item}:')
-    mask = pl.get_request_mask(request_next_item)
-    print(f'mask: {mask}')
-
-    request_next_item = ['val1', ['gradient', [1]], 1] 
+    request_parm, request_code, request_weight = request_next_item
+    pl.set_request_mask(request_parm, request_code)
+    print(f'mask: {pl.request_mask[:pl.current_datasize]}')
+    
+    request_next_item = ['index', ['gradient', [1]], 1] 
     print(f'request mask for {request_next_item}:')
-    mask = pl.get_request_mask(request_next_item)
-    print(f'mask: {mask}')
-
-    request_next_item = ['val1', ['gradient', [2]], 1] 
+    request_parm, request_code, request_weight = request_next_item
+    pl.set_request_mask(request_parm, request_code)
+    print(f'mask: {pl.request_mask[:pl.current_datasize]}')
+    
+    request_next_item = ['index', ['gradient', [2]], 1] 
     print(f'request mask for {request_next_item}:')
-    mask = pl.get_request_mask(request_next_item)
-    print(f'mask: {mask}')
-
+    request_parm, request_code, request_weight = request_next_item
+    pl.set_request_mask(request_parm, request_code)
+    print(f'mask: {pl.request_mask[:pl.current_datasize]}')
+    
     request_next_item = ['val1', ['gradient', [-2]], 1] 
     print(f'request mask for {request_next_item}:')
-    mask = pl.get_request_mask(request_next_item)
-    print(f'mask: {mask}')
+    request_parm, request_code, request_weight = request_next_item
+    pl.set_request_mask(request_parm, request_code)
+    print(f'mask: {pl.request_mask[:pl.current_datasize]}')
 
     request_next_item = ['val1', ['gr_abs', [2]], 1] 
     print(f'request mask for {request_next_item}:')
-    mask = pl.get_request_mask(request_next_item)
-    print(f'mask: {mask}')
+    request_parm, request_code, request_weight = request_next_item
+    pl.set_request_mask(request_parm, request_code)
+    print(f'mask: {pl.request_mask[:pl.current_datasize]}')
 
     request_next_item = ['val1', ['gr_abs', [-2]], 1] 
     print(f'request mask for {request_next_item}:')
-    mask = pl.get_request_mask(request_next_item)
-    print(f'mask: {mask}')
+    request_parm, request_code, request_weight = request_next_item
+    pl.set_request_mask(request_parm, request_code)
+    print(f'mask: {pl.request_mask[:pl.current_datasize]}')
+    
 
 # test
 if __name__ == '__main__' :
-    encoder_test()
+    #encoder_test()
     #basic_test()
-    #request_dev()
+    request_dev()
     # profiling tests
     #import cProfile
     #cProfile.run('pl.get_request_mask(request_next_item)')
