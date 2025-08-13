a3ffee25414c0fcd1667a920dbad2205537d4ca4
Oeyvind <obrandts@gmail.com>
2025-08-13 10:50:57 +0200

    preparing onset comparison

--- test_get_onsets_from_analyzer_midifile.py
+++ test_get_onsets_from_analyzer_midifile.py
@@ -18,38 +18,39 @@ if not len(sys.argv) == 3:
     print('need 2 arguments: midi file name and num events (numevents = -1 use all events)')
     sys.exit()
 
-testfile = './test_data/'+sys.argv[1]
-midifile = MidiFile(testfile)
-print('midi file type', midifile.type)
-numevents = int(sys.argv[2])
-if numevents == -1:
-    numevents = len(testfile)
-print(f'testing with midi read from {testfile} with {numevents} events')
-unused_addr = ''
-i = 0
-delta = 0
-t = 0
-for msg in midifile:
-    if msg.type == 'note_on' and msg.velocity > 0:
-        msg.time += delta
-        t += msg.time
-        print(msg, t)
-        osc_data = (t, 1, msg.note, msg.velocity)
-        m.server.receive_eventdata(unused_addr, *osc_data) # on event
-        osc_data = (t+0.05, 0, msg.note, msg.velocity)
-        m.server.receive_eventdata(unused_addr, *osc_data) # off event
-        delta = 0
-        i += 1
-    else:
-        delta += msg.time
-    if i > numevents:
-        # terminate last phrase
-        osc_data = (-1, -1, 1, 1)
-        m.server.receive_eventdata(unused_addr, *osc_data) # terminate last phrase
-        break
-        
-print(m.dc.pnum_corpus.keys())
+def get_onsets_from_analyzer(testfile):
+    midifile = MidiFile(testfile)
+    print('midi file type', midifile.type)
+    numevents = int(sys.argv[2])
+    if numevents == -1:
+        numevents = len(testfile)
+    print(f'get analyzer onsets from {testfile} with {numevents} events')
+    unused_addr = ''
+    i = 0
+    delta = 0
+    t = 0
+    for msg in midifile:
+        if msg.type == 'note_on' and msg.velocity > 0:
+            msg.time += delta
+            t += msg.time
+            osc_data = (t, 1, msg.note, msg.velocity)
+            m.server.receive_eventdata(unused_addr, *osc_data) # on event
+            osc_data = (t+0.05, 0, msg.note, msg.velocity)
+            m.server.receive_eventdata(unused_addr, *osc_data) # off event
+            delta = 0
+            i += 1
+        else:
+            delta += msg.time
+        if i > numevents:
+            # terminate last phrase
+            osc_data = (-1, -1, 1, 1)
+            m.server.receive_eventdata(unused_addr, *osc_data) # terminate last phrase
+            break
+    return m.dc.corpus[:numevents+1,4]
 
-# print corpus
-with np.printoptions(precision=2, suppress=True):
-    print(m.dc.corpus[:20])
+#
+testfile = sys.argv[1]
+analyzer_onsets = get_onsets_from_analyzer(testfile)
+print('analyzer_onsets \n', analyzer_onsets)
+
+
